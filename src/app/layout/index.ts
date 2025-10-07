a8ed1bf5fc19043691ab3b2ef3a84cd098571e15
7s.i <7s.i@pm.me>
2025-10-07 17:47:15 +0800

    更新佈局組件導出路徑，將基本佈局組件更名為基本佈局，並調整護照佈局中的國際化組件導入路徑，以提升專案結構的清晰度與可維護性。

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -1,3 +1,3 @@
-export * from './basic/basic.component';
+export * from './basic-layout/basic.component';
 export * from './blank-layout/blank.component';
 export * from './passport-layout/passport.component';
